abfaeedbf463fc1fa8b6867f064a819c3c424e07
BrandonAngelesUMBC <brandoa1@umbc.edu>
2026-03-03 02:52:20 +0000

    final touches

--- mytest.c
+++ mytest.c
@@ -2,24 +2,24 @@
 #include <string.h>
 #include <stdbool.h>
 
-extern int sum_ints(int *arr, int size);                       // Assembly function with two parameters and int return type
+extern int sum_ints(int *arr, int size);                        // Assembly function with two parameters and int return type
 int main()
 {
     FILE *dataFile;
     bool validFile = false;
-    char fileName[100];                                // Initialize fileName 
+    char fileName[100];                                         // Initialize fileName 
 
     while(!validFile){
-        printf("Enter file name: ");                   // Prompt for user input
+        printf("Enter file name: ");                            // Prompt for user input
         fgets(fileName, sizeof(fileName), stdin);
-        fileName[strcspn(fileName, "\n")] = '\0';      // Remove "\n" from the string to prevent extra bits
+        fileName[strcspn(fileName, "\n")] = '\0';               // Remove "\n" from the string to prevent extra bits
 
         dataFile = fopen(fileName, "r");
 
-        if(dataFile == NULL){
+        if(dataFile == NULL){                                   // If unable to open text file, prompt user again
             printf("Error opening file\n");
         }else{
-            printf("Reading data from file..\n");
+            printf("Reading data from file..\n");               // When valid file opened, exit while loop
             validFile = true;
         }
     }
@@ -28,23 +28,20 @@ int main()
     int counter = 0;
     int currNum = 0;
     
-    if(fscanf(dataFile, "%d", &currNum) == 1){
-        totalNums = currNum;
-    }
+    fscanf(dataFile, "%d", &totalNums);                         // Total nums obtained from first line of text file
 
-    int nums[totalNums];
+    int nums[totalNums];                                        // Initialize integer array of size totalNums
 
-    while(fscanf(dataFile, "%d", &currNum) == 1){
-        nums[counter] = currNum;
+    while(fscanf(dataFile, "%d", &currNum) == 1){               // Store the following nums in the array
+        nums[counter] = currNum;   
         counter++;
     }
 
-    // Close the file
-    fclose(dataFile);
+    fclose(dataFile);                                           // Close the file
 
-    int sum = sum_ints(nums, totalNums);
+    int sum = sum_ints(nums, totalNums);                        // Call the Assembly function sum_ints
 
-    printf("Total sum: %d\n", sum);
+    printf("Total sum: %d\n", sum);                             // Print returned sum
 
     return 0;
 }
